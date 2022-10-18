5e66ec0a095349d851fd4aef882d6bc2c59e4beb
김세현 <dnjfdid14@naver.com>
2022-10-18 19:59:59 +0900

    Main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import os
 import cv2
-import random
-import pickle
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -30,6 +28,7 @@ cnt = 0
 imgS = []
 classNum = []
 mylist = os.listdir(path)
+numOfClass = len(mylist)
 print("total classes :", len(mylist))
 
 for i in range(0, len(mylist) - 1):
@@ -47,3 +46,25 @@ classNum = np.array(classNum)
 
 x_train, x_test, y_train, y_test = train_test_split(imgS, classNum, test_size=ratio)
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=val_ratio)
+
+print("Data Shapes")
+print("Train", end="")
+print(x_train.shape, y_train.shape)
+print("Validation", end="")
+print(x_val.shape, y_val.shape)
+print("Test", end="")
+print(x_test.shape, y_test.shape)
+assert(x_train.shape[0] == y_train.shape[0])
+assert(x_val.shape[0] == y_val.shape[0])
+assert(x_test.shape[0] == y_test.shape[0])
+assert(x_train.shape[1:] == (img_dim))
+assert(x_val.shape[1:] == (img_dim))
+assert(x_test.shape[1:] == (img_dim))
+
+data=pd.read_csv(label)
+print("data shape", data.shape, type(data))
+
+numSamples = []
+cols = 5
+numClass = numOfClass
+fig, axs = plt.subplots()
